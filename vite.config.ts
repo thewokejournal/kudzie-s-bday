a628debf7fe69e683aae0d0081200ebc04a2af75
thewokejournal <thewokejournal@gmail.com>
2025-12-29 16:43:44 +0000

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/kudzie-s-b-day/',
+      base: '/kudzie-s-bday/',
       server: {
         port: 3000,
         host: '0.0.0.0',
